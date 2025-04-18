7fe0ce283c07bd47bd2488b7b64e38f4c5e37239
rchong5 <stealth@alum.wpi.edu>
2025-04-18 14:07:08 -0400

    added database setup playground script

---
+++ bootstrap.mongodb.js
@@ -0,0 +1,87 @@
+/* global use, db */
+// MongoDB Playground
+// To disable this template go to Settings | MongoDB | Use Default Template For Playground.
+// Make sure you are connected to enable completions and to be able to run a playground.
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+// The result of the last command run in a playground is shown on the results panel.
+// By default the first 20 documents will be returned with a cursor.
+// Use 'console.log()' to print to the debug output.
+// For more documentation on playgrounds please refer to
+// https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
+
+// Select the database to use.
+use('segmenter');
+
+
+
+
+
+db.getCollection("staff").insertMany([
+  {"UUID": "9050477f-55ae-4e6a-8968-316065eeeff7", "name": "Jill"},
+  {"UUID": "d46bf151-7d4d-4d75-b9ff-d8223acbf2a0", "name": "Jack"},
+]);
+
+
+
+
+
+db.getCollection("projects").insertMany([
+  {"UUID": "bc5396ab-d72f-4f01-b995-f2af6bf87ecd", "name": "kittens", "annotated": 100,  "imgs": ["53c2e409-389c-4a9d-9f97-616c58a3f24f"]},
+  {"UUID": "25322e48-8a91-4d9d-886c-b74aa2de9f86", "name": "puppies", "annotated": 100,  "imgs": ["3f7d1d1c-c14e-4b5c-aab3-4ba81839c499"] },
+]);
+
+
+
+
+
+db.getCollection("images").insertMany([
+  {
+    "UUID": "53c2e409-389c-4a9d-9f97-616c58a3f24f",
+    "URL": "https://aquitainevet.com/wp-content/uploads/2018/06/aquitaine-kittens.jpg",
+    "annotations": ["b8dc0730-f8f1-4280-b9c6-c1b7d52ca44d", "0d88f764-2cf7-4c77-b1e8-e27eca7cc4eb"]
+  },
+  {
+    "UUID": "3f7d1d1c-c14e-4b5c-aab3-4ba81839c499",
+    "URL": "https://cdn.britannica.com/55/164255-050-AAADAF11/litter-Australian-Shepherd-puppies.jpg",
+    "annotations": ["178cb145-53e3-417a-9ae0-cf0eca9aafc5", "a928bc70-2946-4910-8e2a-1bebe3c453ff"]
+  },
+]);
+
+
+
+
+
+db.getCollection("annotations").insertMany([
+  {
+    "UUID": "b8dc0730-f8f1-4280-b9c6-c1b7d52ca44d",
+    "annotators": ["9050477f-55ae-4e6a-8968-316065eeeff7"],
+    "label": "meow",
+    "area": 1238,
+    "points": [{"x": 70, "y": 125}, {"x": 80, "y": 140}, {"x": 100, "y": 155}, {"x": 80, "y": 200}, {"x": 65, "y": 155}]
+  },
+  {
+    "UUID": "0d88f764-2cf7-4c77-b1e8-e27eca7cc4eb",
+    "annotators": ["9050477f-55ae-4e6a-8968-316065eeeff7"],
+    "label": "litter",
+    "area": 2475,
+    "points": [{"x": 140, "y": 145}, {"x": 160, "y": 160}, {"x": 200, "y": 175}, {"x": 160, "y": 220}, {"x": 130, "y": 175}]
+  },
+  {
+    "UUID": "178cb145-53e3-417a-9ae0-cf0eca9aafc5",
+    "annotators": ["d46bf151-7d4d-4d75-b9ff-d8223acbf2a0"],
+    "label": "arf",
+    "area": 1063,
+    "points": [{"x": 40, "y": 105}, {"x": 50, "y": 120}, {"x": 70, "y": 135}, {"x": 50, "y": 170}, {"x": 35, "y": 135}]
+  },
+  {
+    "UUID": "a928bc70-2946-4910-8e2a-1bebe3c453ff",
+    "annotators": ["d46bf151-7d4d-4d75-b9ff-d8223acbf2a0"],
+    "label": "ball",
+    "area": 3188,
+    "points": [{"x": 120, "y": 165}, {"x": 150, "y": 180}, {"x": 210, "y": 195}, {"x": 150, "y": 230}, {"x": 105, "y": 195}]
+  },
+]);
+
+
+
+
