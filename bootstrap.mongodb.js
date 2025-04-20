b1a2cfa19cd7ce4cd6c7675e940851f667b61536
rchong5 <stealth@alum.wpi.edu>
2025-04-20 13:00:58 -0400

    switched UUID data from strings to BSON

--- bootstrap.mongodb.js
+++ bootstrap.mongodb.js
@@ -17,8 +17,8 @@ use('segmenter');
 
 
 db.getCollection("staff").insertMany([
-  {"UUID": "9050477f-55ae-4e6a-8968-316065eeeff7", "name": "Jill"},
-  {"UUID": "d46bf151-7d4d-4d75-b9ff-d8223acbf2a0", "name": "Jack"},
+   {_id: UUID("9050477f-55ae-4e6a-8968-316065eeeff7"), "name": "Jill"},
+   {_id: UUID("d46bf151-7d4d-4d75-b9ff-d8223acbf2a0"), "name": "Jack"},
 ]);
 
 
@@ -26,8 +26,8 @@ db.getCollection("staff").insertMany([
 
 
 db.getCollection("projects").insertMany([
-  {"UUID": "bc5396ab-d72f-4f01-b995-f2af6bf87ecd", "name": "kittens", "annotated": 100,  "imgs": ["53c2e409-389c-4a9d-9f97-616c58a3f24f"]},
-  {"UUID": "25322e48-8a91-4d9d-886c-b74aa2de9f86", "name": "puppies", "annotated": 100,  "imgs": ["3f7d1d1c-c14e-4b5c-aab3-4ba81839c499"] },
+  {_id: UUID("bc5396ab-d72f-4f01-b995-f2af6bf87ecd"), "name": "kittens", "annotated": 100,  "imgs": [UUID("53c2e409-389c-4a9d-9f97-616c58a3f24f")]},
+  {_id: UUID("25322e48-8a91-4d9d-886c-b74aa2de9f86"), "name": "puppies", "annotated": 100,  "imgs": [UUID("3f7d1d1c-c14e-4b5c-aab3-4ba81839c499")] },
 ]);
 
 
@@ -36,14 +36,14 @@ db.getCollection("projects").insertMany([
 
 db.getCollection("images").insertMany([
   {
-    "UUID": "53c2e409-389c-4a9d-9f97-616c58a3f24f",
+    _id: UUID("53c2e409-389c-4a9d-9f97-616c58a3f24f"),
     "URL": "https://aquitainevet.com/wp-content/uploads/2018/06/aquitaine-kittens.jpg",
-    "annotations": ["b8dc0730-f8f1-4280-b9c6-c1b7d52ca44d", "0d88f764-2cf7-4c77-b1e8-e27eca7cc4eb"]
+    "annotations": [UUID("b8dc0730-f8f1-4280-b9c6-c1b7d52ca44d"), UUID("0d88f764-2cf7-4c77-b1e8-e27eca7cc4eb")]
   },
   {
-    "UUID": "3f7d1d1c-c14e-4b5c-aab3-4ba81839c499",
+    _id: UUID("3f7d1d1c-c14e-4b5c-aab3-4ba81839c499"),
     "URL": "https://cdn.britannica.com/55/164255-050-AAADAF11/litter-Australian-Shepherd-puppies.jpg",
-    "annotations": ["178cb145-53e3-417a-9ae0-cf0eca9aafc5", "a928bc70-2946-4910-8e2a-1bebe3c453ff"]
+    "annotations": [UUID("178cb145-53e3-417a-9ae0-cf0eca9aafc5"), UUID("a928bc70-2946-4910-8e2a-1bebe3c453ff")]
   },
 ]);
 
@@ -53,29 +53,29 @@ db.getCollection("images").insertMany([
 
 db.getCollection("annotations").insertMany([
   {
-    "UUID": "b8dc0730-f8f1-4280-b9c6-c1b7d52ca44d",
-    "annotators": ["9050477f-55ae-4e6a-8968-316065eeeff7"],
+    _id: UUID("b8dc0730-f8f1-4280-b9c6-c1b7d52ca44d"),
+    "annotators": [UUID("9050477f-55ae-4e6a-8968-316065eeeff7")],
     "label": "meow",
     "area": 1238,
     "points": [{"x": 70, "y": 125}, {"x": 80, "y": 140}, {"x": 100, "y": 155}, {"x": 80, "y": 200}, {"x": 65, "y": 155}]
   },
   {
-    "UUID": "0d88f764-2cf7-4c77-b1e8-e27eca7cc4eb",
-    "annotators": ["9050477f-55ae-4e6a-8968-316065eeeff7"],
+    _id: UUID("0d88f764-2cf7-4c77-b1e8-e27eca7cc4eb"),
+    "annotators": [UUID("9050477f-55ae-4e6a-8968-316065eeeff7")],
     "label": "litter",
     "area": 2475,
     "points": [{"x": 140, "y": 145}, {"x": 160, "y": 160}, {"x": 200, "y": 175}, {"x": 160, "y": 220}, {"x": 130, "y": 175}]
   },
   {
-    "UUID": "178cb145-53e3-417a-9ae0-cf0eca9aafc5",
-    "annotators": ["d46bf151-7d4d-4d75-b9ff-d8223acbf2a0"],
+    _id: UUID("178cb145-53e3-417a-9ae0-cf0eca9aafc5"),
+    "annotators": [UUID("d46bf151-7d4d-4d75-b9ff-d8223acbf2a0")],
     "label": "arf",
     "area": 1063,
     "points": [{"x": 40, "y": 105}, {"x": 50, "y": 120}, {"x": 70, "y": 135}, {"x": 50, "y": 170}, {"x": 35, "y": 135}]
   },
   {
-    "UUID": "a928bc70-2946-4910-8e2a-1bebe3c453ff",
-    "annotators": ["d46bf151-7d4d-4d75-b9ff-d8223acbf2a0"],
+    _id: UUID("a928bc70-2946-4910-8e2a-1bebe3c453ff"),
+    "annotators": [UUID("d46bf151-7d4d-4d75-b9ff-d8223acbf2a0")],
     "label": "ball",
     "area": 3188,
     "points": [{"x": 120, "y": 165}, {"x": 150, "y": 180}, {"x": 210, "y": 195}, {"x": 150, "y": 230}, {"x": 105, "y": 195}]
