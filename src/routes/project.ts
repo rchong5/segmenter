8ddb9e87e483364c4d1c1f720c92ea1e8ad59058
rchong5 <stealth@alum.wpi.edu>
2025-04-20 14:44:07 -0400

    updated project end--points to support fetching by one or more project names

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -10,7 +10,33 @@ const projectRouter = express.Router();
 
 
 
-const fetchProject = async (UUIDIn: string): Promise<Project> => 
+const fetchProjectByName = async (nameIn: string): Promise<Project> => 
+{
+  const mdb = await connectDB();
+
+  const collection = mdb.collection("projects");
+
+  const result = await collection.findOne({ name: nameIn });
+
+  let UUIDResult: Project = { _id: '', name: '', annotated: 0, imgs: [] };
+
+  if(result && '_id' in result && 'name' in result && 'annotated' in result && 'imgs' in result)
+  {
+    UUIDResult = { _id: result._id.toString(), name: result.name, annotated: result.annotated, imgs: result.imgs } as Project;
+  }
+  else
+  {
+    console.warn('Result does not match Project interface:', result);
+  }
+
+  return(UUIDResult);
+}
+
+
+
+
+
+const fetchProjectByUUID = async (UUIDIn: string): Promise<Project> => 
 {
   const UUID = MUUID.from(UUIDIn);
  
@@ -38,6 +64,42 @@ const fetchProject = async (UUIDIn: string): Promise<Project> =>
 
 
 
+// get projects by name or multiple comma-separated names
+// returns a JSON array of projects
+projectRouter.get("/name/:name", async (req, res, next) =>
+  {
+    const names: string[] = req.params.name.split(",").map(name => name.trim());
+  
+    let projectOut: Project[] = [];
+  
+    try
+    {
+      for(const nameStr of names)
+      {
+        console.log("Attempting to fetch projects by name => " + nameStr);
+  
+        const projectResult = await fetchProjectByName(nameStr);
+        projectOut.push(projectResult);
+      }
+  
+      res.json(projectOut);
+    }
+    catch (error)
+    {
+      console.error('Error fetching project by UUID:', error);
+      res.status(500).json({ error: 'Failed to fetch project by UUID' });
+      return next(error);
+    }
+    finally
+    {
+       await closeDB();
+    }
+  });
+
+
+
+
+
 // get projects by UUID or multiple comma-separated UUIDs
 // returns a JSON array of projects
 projectRouter.get("/UUID/:UUID", async (req, res, next) =>
@@ -52,7 +114,7 @@ projectRouter.get("/UUID/:UUID", async (req, res, next) =>
     {
       console.log("Attempting to fetch projects by uuid => " + UUIDStr);
 
-      const projectResult = await fetchProject(UUIDStr);
+      const projectResult = await fetchProjectByUUID(UUIDStr);
       projectOut.push(projectResult);
     }
 
@@ -69,5 +131,9 @@ projectRouter.get("/UUID/:UUID", async (req, res, next) =>
      await closeDB();
   }
 });
+
+
+
+
 
 export default projectRouter;
