126ddde507ad289cc6eb39df10629dff2a34fb09
rchong5 <stealth@alum.wpi.edu>
2025-04-21 15:17:27 -0400

    updated annotations router to support fetching an annotation by specifying a label

--- src/routes/annotation.ts
+++ src/routes/annotation.ts
@@ -10,15 +10,13 @@ const annotationRouter = express.Router();
 
 
 
-export const fetchAnnotation = async (UUIDIn: string): Promise<Annotation> => 
+export const fetchAnnotationByLabel = async (labelIn: string): Promise<Annotation> => 
 {
-  const UUID = MUUID.from(UUIDIn);
-
   const mdb = await connectDB();
 
   const collection = mdb.collection("annotations");
 
-  const result = await collection.findOne({ _id: UUID });
+  const result = await collection.findOne({ label: labelIn });
 
   let UUIDResult: Annotation = { _id: '', annotators: [], img: '', projects: [], label: '', area: 0, points: [] };
 
@@ -47,6 +45,75 @@ export const fetchAnnotation = async (UUIDIn: string): Promise<Annotation> =>
 
 
 
+export const fetchAnnotationByUUID = async (UUIDIn: string): Promise<Annotation> => 
+  {
+    const UUID = MUUID.from(UUIDIn);
+  
+    const mdb = await connectDB();
+  
+    const collection = mdb.collection("annotations");
+  
+    const result = await collection.findOne({ _id: UUID });
+  
+    let UUIDResult: Annotation = { _id: '', annotators: [], img: '', projects: [], label: '', area: 0, points: [] };
+  
+    if(result && '_id' in result && 'annotators' in result && 'img' in result && 'projects' in result && 'label' in result && 'area' in result && 'points' in result)
+    {
+      UUIDResult =
+        {
+          _id: result._id.toString(),
+          annotators: result.annotators,
+          img: result.img,
+          projects: result.projects,
+          label: result.label,
+          area: result.area,
+          points: result.points
+        } as Annotation;
+    }
+    else
+    {
+      console.warn('Result does not match Annotation interface:', result);
+    }
+  
+    return(UUIDResult);
+  }
+
+
+
+
+// get annotations by label
+// returns a JSON array of annotations
+annotationRouter.get("/label/:label", async (req, res, next) =>
+{
+//    const UUIDs: string[] = req.params.UUID.split(",").map(UUID => UUID.trim());
+  
+    let annotationOut: Annotation[] = [];
+  
+    try
+    {
+      console.log("Attempting to fetch annotations by label => " + req.params.label);
+  
+      const annotationResult = await fetchAnnotationByLabel(req.params.label);
+      annotationOut.push(annotationResult);
+  
+      res.json(annotationOut);
+    }
+    catch (error)
+    {
+      console.error('Error fetching annotation by label:', error);
+      res.status(500).json({ error: 'Failed to fetch annotation by label' });
+      return next(error);
+    }
+    finally
+    {
+       await closeDB();
+    }
+});
+
+
+
+
+
 // get annotations by UUID or multiple comma-separated UUIDs
 // returns a JSON array of annotations
 annotationRouter.get("/UUID/:UUID", async (req, res, next) =>
@@ -61,7 +128,7 @@ annotationRouter.get("/UUID/:UUID", async (req, res, next) =>
     {
       console.log("Attempting to fetch annotations by uuid => " + UUIDStr);
 
-      const annotationResult = await fetchAnnotation(UUIDStr);
+      const annotationResult = await fetchAnnotationByUUID(UUIDStr);
       annotationOut.push(annotationResult);
     }
 
